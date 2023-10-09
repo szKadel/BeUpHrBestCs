ebc5245bcf83445038b299376ef70b76105b92b4
Szymon Kadelski <kontakt@szymonkadelski.pl>
2023-10-09 17:37:32 +0200

    Mailer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,29 @@
 
 namespace App\Controller;
 
+use App\Service\EmailService;
+use http\Client\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/',name: 'app_main_mainpage')]
     public function mainPage(): RedirectResponse
     {
         return new RedirectResponse('/view/');
+    }
+
+    #[Route('/mail/')]
+    public function sendEmail(EmailService $emailService): Response
+    {
+        $subject = 'Temat wiadomości';
+        $to = 'szymon@beupsoft.pl';
+        $body = 'Treść wiadomości.';
+
+        $emailService->sendEmail($subject, $to, $body);
+
+        return new Response("test");
     }
 }
---
+++ src/Service/EmailService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class EmailService
+{
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function sendEmail(string $subject, string $to, string $body): void
+    {
+        $email = (new Email())
+            ->from('beuphr@beupsoft.pl')
+            ->to($to)
+            ->subject($subject)
+            ->html($body);
+
+        $this->mailer->send($email);
+    }
+}
