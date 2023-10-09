0fd411467be836bbb8048613afaa69631d23d5cc
Szymon Kadelski <kontakt@szymonkadelski.pl>
2023-10-09 15:13:28 +0200

    Init Commit

---
+++ src/Entity/Settings/Notifications.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity\Settings;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\Settings\NotificationsRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: NotificationsRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(normalizationContext: ['groups' => ['notificationSetting:read']],security: "is_granted('ROLE_ADMIN')"),
+        new Put(denormalizationContext: ['groups' => ['notificationSetting:update']],security: "is_granted('ROLE_ADMIN')")
+    ],
+    paginationClientItemsPerPage: true,
+    paginationItemsPerPage: 7,
+)]
+class Notifications
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Groups(['notificationSetting:read', 'notificationSetting:update'])]
+    private ?bool $NotifcateAdminOnAcceptVacation = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Groups(['notificationSetting:read', 'notificationSetting:update'])]
+    private ?bool $NotificateDepartmentModOnCreatedVacation = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Groups(['notificationSetting:read', 'notificationSetting:update'])]
+    private ?bool $NotificateReplacmentUser = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Groups(['notificationSetting:read', 'notificationSetting:update'])]
+    private ?bool $NotificateUserOnVacationRequestAccept = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isNotifcateAdminOnAcceptVacation(): ?bool
+    {
+        return $this->NotifcateAdminOnAcceptVacation;
+    }
+
+    public function setNotifcateAdminOnAcceptVacation(?bool $NotifcateAdminOnAcceptVacation): static
+    {
+        $this->NotifcateAdminOnAcceptVacation = $NotifcateAdminOnAcceptVacation;
+
+        return $this;
+    }
+
+    public function isNotificateDepartmentModOnCreatedVacation(): ?bool
+    {
+        return $this->NotificateDepartmentModOnCreatedVacation;
+    }
+
+    public function setNotificateDepartmentModOnCreatedVacation(?bool $NotificateDepartmentModOnCreatedVacation): static
+    {
+        $this->NotificateDepartmentModOnCreatedVacation = $NotificateDepartmentModOnCreatedVacation;
+
+        return $this;
+    }
+
+    public function isNotificateReplacmentUser(): ?bool
+    {
+        return $this->NotificateReplacmentUser;
+    }
+
+    public function setNotificateReplacmentUser(?bool $NotificateReplacmentUser): static
+    {
+        $this->NotificateReplacmentUser = $NotificateReplacmentUser;
+
+        return $this;
+    }
+
+    public function isNotificateUserOnVacationRequestAccept(): ?bool
+    {
+        return $this->NotificateUserOnVacationRequestAccept;
+    }
+
+    public function setNotificateUserOnVacationRequestAccept(?bool $NotificateUserOnVacationRequestAccept): static
+    {
+        $this->NotificateUserOnVacationRequestAccept = $NotificateUserOnVacationRequestAccept;
+
+        return $this;
+    }
+}
---
+++ src/State/EmployeeStateProcessor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\Company\Employee;
+use App\Entity\Vacation\Vacation;
+
+class EmployeeStateProcessor implements ProcessorInterface
+{
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
+    {
+        if($data instanceof Employee) {
+            if ($operation instanceof Post) {
+                $this->addEmployee();
+            }
+        }
+    }
+
+    private function addEmployee()
+    {
+
+    }
+}
