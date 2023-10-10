54e441017745881af17acae838c9dd0b55a3046f
Szymon <kontakt@szymonkadelski.pl>
2023-10-11 00:53:00 +0200

    Mail Test

--- src/EventSubscriber/EmployeeSubscriber.php
+++ src/EventSubscriber/EmployeeSubscriber.php
@@ -26,7 +26,7 @@ final class EmployeeSubscriber implements EventSubscriberInterface
     }
     public function sendMail(ViewEvent $event): void
     {
-        $book = $event->getResponse();
+        $book = $event->getResponse()->getContent();
         $method = $event->getRequest()->getMethod();
 
         $this->mailer ->sendEmail("Test","szymonkadelski@gmail.com","test ".json_encode($book));
