80d66b394df1b1578a9970d74723665627d52d79
Szymon Kadelski <kontakt@szymonkadelski.pl>
2023-10-13 16:14:50 +0200

    Notification -> update -> refactor

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -48,7 +48,7 @@ class EmailService
 
     public function sendReplacementEmployeeNotification(Employee $employee, Employee $replacementUser):void
     {
-        if (!empty($replacementUser->getEmail())) {
+        if (!empty($replacementUser->getUser()->getEmail())) {
             $this->sendEmail(
                 "Bestcs Hr - powiadomienie",
                 $replacementUser->getUser()->getEmail(),
